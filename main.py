abbc676ca306e1d3d2db100eaa36565ad4b2c0e1
Abdulloh <163301172+Sasuke0414@users.noreply.github.com>
2024-09-14 16:03:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,113 @@
+
+# Imtihon qoidalari, aldamang, atrofga qaramang!
+# Savollarga javob yozing va na'muna kodini taqdim eting
+
+
+# 1-savol: print() bu...
+
+#Javob:
+    # foydalanuvchiga ma'lumot berish uchun ishlatiladi
+# Kod namunasi:
+    # print("Bugun imtihon")
+
+
+# 2-savol: input() bu...
+
+#Javob:
+# foydalanuvchida malumot olish 
+# Kod namunasi:
+# input("Son kiriting !")
+
+# 3-savol: O'zgaruvchi nima?
+
+#Javob:
+# ozgaruvchi bu karobka dsek ham boladi malumotni esa luboy narsa 
+# Kod namunasi:
+# Yosh = input("Yoshingiz ?")
+
+# 4-savol: 3 ta maʼlumot turini sanab oʻting va misol keltiring:
+
+#Javob:
+# string, number, booling 
+# Kod namunasi:
+
+
+# 5-savol: Format string nima (f"")?
+
+#Javob:
+# stringni formatlash 
+# Kod namunasi:
+# text = print("5")
+# oxri = print(f"{text}" * 5)
+# 6-savol: Mantiqiy operatorlar nima?
+
+#Javob:
+# or and 
+# Kod namunasi:
+# true and false => False
+# true or false => True
+# 7-savol: Taqqoslash operatorlarini sanab bering va ularga misol keltiring:
+
+#Javob:
+# or 
+# Kod namunasi:
+# 3 or 5 = 5
+
+# 8-savol: If, elif, else nima va ular bilan misol ko'ring:
+
+#Javob:
+# buni kopincha shart berishda ishlatiladi 
+# Kod namunasi:
+# if 5 == 5:
+# else:
+#     print("Zor")
+# 9-savol: For nima:
+
+#Javob:
+# for aylanb chiqish 
+# Kod namunasi:
+
+# for i in range(10):
+#     print("Salom")
+
+# for i in str(10):
+#     print("Abdulloh")
+
+# 9-savol: While nima:
+
+#Javob:
+# for ning sikli lekn sentacsis farq qladi 
+# Kod namunasi:
+# while 5 > 3:
+#     print("Salom")
+
+# 10-savol: For va while o'rtasidagi farq nima?
+
+#Javob:
+# sentacisi farq qladi br bridan 
+# Kod namunasi:
+# while 3 == 3:
+#     print("abdulloh")
+
+# for i in range(10):
+#     print("Salom")
+
+# 11-chi: Endi siz  tosh, qog'oz, qaychi o'yinini yaratishingiz kerak.
+    
+# amaliy 
+
+# player1 = input("Player1 (tosh/qaychi/qog'oz): ")
+# player2 = input("Player2 (tosh/qaychi/qog'oz): ")
+
+# if player1 == "tosh" and player2 == "qaychi":
+#     print("Player1 yutdi")
+# elif player1 == "qaychi" and player2 == "tosh":
+#     print("Player2 yutdi!")
+# elif player1 == "tosh" and player2 == "qog'oz":
+#     print("Player2 yutdi!")
+# elif player1 == "qog'oz" and player2 == "tosh":
+#     print("Player2 yutdi!")
+# elif player1 == "qog'oz" and player2 == "qaychi":
+#     print("Player2 yutdi!")
+# elif player1 == "qaychi" and player2 == "qog'oz":
+#     print("Player2 yutdi!")
